8c58cb0cdb8f23a7795d80d4b0390cb8f90aaf74
Bishar Abdinur Mohamed <134773924+bisharr@users.noreply.github.com>
2023-11-01 07:07:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+const btnClickEl = document.querySelector('.clickBtn');
+const btnlessEl = document.querySelector('.btnless');
+const hidingPricesEl = document.querySelector('.hiding-prices');
+
+const feuturingEl = function () {
+  hidingPricesEl.classList.toggle('hide');
+  btnlessEl.classList.remove('hide');
+  btnClickEl.classList.add('hide');
+};
+const btnless2 = function () {
+  hidingPricesEl.classList.toggle('hide');
+  btnlessEl.classList.add('hide');
+  btnClickEl.classList.remove('hide');
+};
+
+btnClickEl.addEventListener('click', feuturingEl);
+
+btnlessEl.addEventListener('click', btnless2);
